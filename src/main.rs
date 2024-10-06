703ff00d5905095671f90ae1109f949f2510f12d
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-10-06 16:00:00 +0530

    Delete Todo

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,20 @@ async fn create_todo(app_state:web::Data<AppState>,todo: web::Json<Todo>) -> imp
     HttpResponse::Ok().json(new_todo)
 }
 
+async fn delete_todo(app_state: web::Data<AppState>, todo_id: web::Path<u64>) -> impl Responder {
+    let mut todo_list = app_state.todo_list.lock().unwrap();
+    let id = *todo_id;
+    
+    if let Some(pos) = todo_list.iter().position(|todo| todo.id == id) {
+        todo_list.remove(pos);
+        HttpResponse::Ok().json(format!("Todo with ID {} deleted", id))
+    } else {
+        HttpResponse::NotFound().json(format!("Todo with ID {} not found", id))
+    }
+}
+
+
+
 async fn get_todos(app_state:web::Data<AppState>)-> impl Responder{
     let todo_list=app_state.todo_list.lock().unwrap();
     HttpResponse::Ok().json(todo_list.to_vec())
@@ -41,6 +55,8 @@ async fn main()->std::io::Result<()>{
             .app_data(app_state.clone())
             .route("/todos",web::post().to(create_todo))
             .route("/todos",web::get().to(get_todos))
+            .route("/todos/{id}", web::delete().to(delete_todo))
+
     })
     .bind("127.0.0.1:3000")?
     .run()
