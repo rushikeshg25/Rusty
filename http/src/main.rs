effcc01b21ededb12568a10fa1eedca79323b7cb
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-11-05 18:28:41 +0530

    feat: basic tcp setup to receive http reqs

--- http/src/main.rs
+++ http/src/main.rs
@@ -1,3 +1,30 @@
+use std::{
+    io::Read,
+    net::{TcpListener, TcpStream},
+};
+
+#[allow(unused)]
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:6969").expect("Failed to bind Tcp to this address");
+    println!("Listening on 6969");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                std::thread::spawn(|| handle_client(stream));
+            }
+            Err(e) => {
+                eprintln!("Failed to establish connection: {}", e);
+            }
+        }
+    }
+}
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    let bytes_read = stream
+        .read(&mut buffer)
+        .expect("Failed to read from client!");
+    let request = String::from_utf8_lossy(&buffer[..bytes_read]);
+    println!("{:?}", request);
 }
